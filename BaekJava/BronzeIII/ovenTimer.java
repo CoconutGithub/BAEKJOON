d8117ecf609d563bf1ea2961834e774ec811df56
CoconutGithub <khkoh5932@gmail.com>
2024-05-09 23:47:21 +0900

    timer

---
+++ BaekJava/BronzeIII/ovenTimer.java
@@ -0,0 +1,22 @@
+package BaekJava.BronzeIII;
+import java.util.*;
+public class ovenTimer {
+    public static void main(String[] args) {
+        Scanner s=new Scanner(System.in);
+        int h=s.nextInt();
+        int m=s.nextInt();
+
+        int time=s.nextInt();
+        if (m+time>=60){
+            h+=(m+time)/60;
+            if(h>23){
+                h%=24;
+            }
+            m=(m+time)%60;
+        }
+        else {
+            m+=time;
+        }
+        System.out.print(h+" "+m);
+    }
+}
