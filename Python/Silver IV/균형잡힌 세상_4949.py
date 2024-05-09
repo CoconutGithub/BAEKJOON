c75bef0d66152435ca94cebf2e17ef933ebe2ad4
CoconutGithub <khkoh5932@gmail.com>
2024-05-10 00:34:17 +0900

    more stack

--- Python/Silver IV/균형잡힌 세상_4949.py
+++ Python/Silver IV/균형잡힌 세상_4949.py
@@ -1,2 +1,33 @@
 from sys import stdin
 input=stdin.readline
+
+while 1:
+    line=input().rstrip()
+    stack=[]
+    if len(line)==1 and line[0]=='.':
+        break
+    for i in line:
+        if i=='(':
+            stack.append('(')
+        elif i==')':
+            try:
+                if stack[-1]!='(':
+                    break
+                stack.pop()
+            except IndexError:
+                stack.append('error')
+                break
+        elif i=='[':
+            stack.append('[')
+        elif i==']':
+            try:
+                if stack[-1]!='[':
+                    break
+                stack.pop()
+            except IndexError:
+                stack.append('error')
+                break
+    if len(stack)==0:
+        print("yes")
+    else:
+        print("no")
