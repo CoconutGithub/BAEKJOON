79f838041af9cc06e6fe1f6a625da01e9ffc889c
CoconutGithub <khkoh5932@gmail.com>
2024-05-12 22:57:22 +0900

    fast I/O

---
+++ BaekJava/BronzeIV/fastIO_15552.java
@@ -0,0 +1,28 @@
+package BaekJava.BronzeIV;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+public class fastIO_15552 {
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int t = Integer.parseInt(br.readLine());
+        for (int i=0;i<t;i++){
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+
+            int sum = a + b;
+            bw.write(Integer.toString(sum));
+            bw.newLine();
+        }
+        bw.flush();
+        bw.close();
+
+        br.close();
+    }
+}
