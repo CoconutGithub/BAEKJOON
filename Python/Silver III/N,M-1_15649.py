8ce322efeec73133b769770a2dde08353264aaf5
CoconutGithub <khkoh5932@gmail.com>
2024-05-30 10:11:16 +0900

    permutations(순열)

---
+++ Python/Silver III/N,M-1_15649.py
@@ -0,0 +1,18 @@
+from sys import stdin
+input=stdin.readline
+from itertools import permutations
+
+n,m=map(int,input().rstrip().split())
+fac=1
+for i in range(m):
+    fac*=n-i
+l=[i for i in range(1,n+1)]
+
+rs=list(permutations(l,m))#순열 문제 n에서 m개의 숫자 뽑아서 조합
+for i in rs:
+    tmp=str(i)
+    tmp=tmp.replace("(","")
+    tmp=tmp.replace(")","")
+    tmp=tmp.replace(",","")
+    print(tmp)
+
